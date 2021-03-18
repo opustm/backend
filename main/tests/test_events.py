681e1214eaeb52e914f7e8982b442099d0641b96
JVS Windows <josh.vansant99@gmail.com>
2021-03-17 22:22:24 -0500

    initial testing
    5/21 endpoints covered

--- main/tests/test_events.py
+++ main/tests/test_events.py
@@ -7,10 +7,49 @@ class TestEventsSetUp(APITestCase):
     def setUp(self):
         self.fake = Faker()
         self.events_url = "/events/"
-        self.get_team_events_by_teamid = reverse("get_team_events_by_teamid")
-        self.get_user_events_by_userid = reverse("get_user_events_by_userid")
-        self.event_data = { }
+        self.event_data = {
+            "user": 1,
+            "team": 1,
+            "name": "",
+            "start": 1,
+            "end": 1,
+            "details": "",
+            "picture": "",
+            "invited": []
+        }
+        self.event_data_invalid = {
+            "user": 2,
+            "team": 1,
+            "name": "",
+            "start": 1,
+            "end": 1,
+            "details": "",
+            "picture": "",
+            "invited": [2]
+        }
         return super().setUp()
     
     def tearDown(self):
         return super().tearDown()
+
+
+class TestEventsViews(TestEventsSetUp):
+    def test_event_get(self):
+        res=self.client.get(self.events_url)
+        self.assertEqual(res.status_code, 200)
+
+    def test_event_post(self):
+        response=self.client.post(self.events_url, self.event_data, format="json")
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(self.event_data, self.event_data)
+        
+        response2=self.client.post(self.events_url, self.event_data_invalid, format="json")
+        self.assertEqual(response2.status_code, 400)
+
+    def test_event_put(self):
+        response=self.client.put(self.events_url, self.event_data, format="json")
+        self.assertEqual(response.status_code, 405)
+
+    def test_event_delete(self):
+        response=self.client.put(self.events_url, self.event_data, format="json")
+        self.assertEqual(response.status_code, 405)
--- main/tests/test_requests.py
+++ main/tests/test_requests.py
@@ -7,10 +7,37 @@ class TestRequestsSetUp(APITestCase):
     def setUp(self):
         self.fake = Faker()
         self.requests_url = "/requests/"
-        self.get_team_requests_by_teamid = reverse("get_team_requests_by_teamid")
-        self.get_user_requests_by_username = reverse("get_user_requests_by_username")
-        self.requests_data = { }
+        self.request_data = {
+            "message": "",
+            "dateRequested": "1234"
+        }
+        self.request_data_invalid = {
+            "message": "hello",
+            "dateRequested": "1234"
+        }
         return super().setUp()
     
     def tearDown(self):
         return super().tearDown()
+
+
+class TestRequestsViews(TestRequestsSetUp):
+    def test_request_get(self):
+        res=self.client.get(self.requests_url)
+        self.assertEqual(res.status_code, 200)
+
+    def test_request_post(self):
+        response=self.client.post(self.requests_url, self.request_data, format="json")
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(self.request_data, self.request_data)
+        
+        response2=self.client.post(self.requests_url, self.request_data_invalid, format="json")
+        self.assertEqual(response2.status_code, 400)
+
+    def test_request_put(self):
+        response=self.client.put(self.requests_url, self.request_data, format="json")
+        self.assertEqual(response.status_code, 405)
+
+    def test_request_delete(self):
+        response=self.client.put(self.requests_url, self.request_data, format="json")
+        self.assertEqual(response.status_code, 405)
