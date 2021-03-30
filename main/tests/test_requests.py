4c6687911af52675706f77630948dc45affba752
JVS Windows <josh.vansant99@gmail.com>
2021-03-30 09:30:17 -0500

    all tests running
    After fixing the bugs that the tests uncovered, we are finally running and passing all of our tests.

--- main/tests/test_requests.py
+++ main/tests/test_requests.py
@@ -64,10 +64,10 @@ class TestRequestsSetUp(APITestCase):
 class TestRequestsViews(TestRequestsSetUp):
 
     def post(self):
-        a=self.client.post("/teams/", self.team_data, format="json")
-        b=self.client.post("/users/", self.user_data, format="json")
-        c=self.client.post("/events/", self.event_data, format="json")
-        d=self.client.post(self.requests_url, self.request_data, format="json")
+        self.client.post("/teams/", self.team_data, format="json")
+        self.client.post("/users/", self.user_data, format="json")
+        self.client.post("/events/", self.event_data, format="json")
+        self.client.post(self.requests_url, self.request_data, format="json")
     
     # requests
     def test_request_get(self):
@@ -75,11 +75,14 @@ class TestRequestsViews(TestRequestsSetUp):
         self.assertEqual(res.status_code, 200)
 
     def test_request_post(self):
-        a=self.client.post("/teams/", self.team_data, format="json")
-        b=self.client.post("/users/", self.user_data, format="json")
-        c=self.client.post("/events/", self.event_data, format="json")
+        self.client.post("/teams/", self.team_data, format="json")
+        self.client.post("/users/", self.user_data, format="json")
+        self.client.post("/events/", self.event_data, format="json")
         response = self.client.post(self.requests_url, self.request_data, format="json")
         self.assertEqual(response.status_code, 201)
+
+        invalid_response = self.client.post(self.requests_url, self.request_data_invalid, format="json")
+        self.assertEqual(invalid_response.status_code, 400)
 
     def test_request_put(self):
         response = self.client.put(self.requests_url, self.request_data, format="json")
@@ -89,58 +92,58 @@ class TestRequestsViews(TestRequestsSetUp):
         response = self.client.delete(self.requests_url)
         self.assertEqual(response.status_code, 405)
 
-    # # requests by id
-    # def test_request_by_id_get(self):
-    #     self.post()
-    #     res = self.client.get(self.requests_by_id(1))
-    #     self.assertEqual(res.status_code, 200)
+    # requests by id
+    def test_request_by_id_get(self):
+        self.post()
+        res = self.client.get(self.requests_by_id(1))
+        self.assertEqual(res.status_code, 200)
 
-    # def test_request_by_id_post(self):
-    #     response = self.client.post(self.requests_by_id(1), self.request_data, format="json")
-    #     self.assertEqual(response.status_code, 405)
+    def test_request_by_id_post(self):
+        response = self.client.post(self.requests_by_id(1), self.request_data, format="json")
+        self.assertEqual(response.status_code, 405)
 
-    # def test_request_by_id_put(self):
-    #     self.post()
-    #     response = self.client.put(self.requests_by_id(1), self.request_data, format="json")
-    #     self.assertEqual(response.status_code, 204)
+    def test_request_by_id_put(self):
+        self.post()
+        response = self.client.put(self.requests_by_id(1), self.request_data, format="json")
+        self.assertEqual(response.status_code, 200)
 
-    # def test_request_by_id_delete(self):
-    #     self.post()
-    #     response = self.client.put(self.requests_by_id(1), self.request_data, format="json")
-    #     self.assertEqual(response.status_code, 205)    
+    def test_request_by_id_delete(self):
+        self.post()
+        response = self.client.put(self.requests_by_id(1), self.request_data, format="json")
+        self.assertEqual(response.status_code, 200)    
     
-    # # requests by team id
-    # def test_request_by_team_id_get(self):
-    #     self.post()
-    #     res = self.client.get(self.requests_by_team_id(1))
-    #     self.assertEqual(res.status_code, 200)
+    # requests by team id
+    def test_request_by_team_id_get(self):
+        self.post()
+        res = self.client.get(self.requests_by_team_id(1))
+        self.assertEqual(res.status_code, 200)
 
-    # def test_request_by_team_id_post(self):
-    #     response = self.client.post(self.requests_by_team_id(1), self.request_data, format="json")
-    #     self.assertEqual(response.status_code, 405)
+    def test_request_by_team_id_post(self):
+        response = self.client.post(self.requests_by_team_id(1), self.request_data, format="json")
+        self.assertEqual(response.status_code, 405)
 
-    # def test_request_by_team_id_put(self):
-    #     response = self.client.put(self.requests_by_team_id(1), self.request_data, format="json")
-    #     self.assertEqual(response.status_code, 405)
+    def test_request_by_team_id_put(self):
+        response = self.client.put(self.requests_by_team_id(1), self.request_data, format="json")
+        self.assertEqual(response.status_code, 405)
 
-    # def test_request_by_team_id_delete(self):
-    #     response = self.client.delete(self.requests_by_team_id(1), self.request_data, format="json")
-    #     self.assertEqual(response.status_code, 405)      
+    def test_request_by_team_id_delete(self):
+        response = self.client.delete(self.requests_by_team_id(1), self.request_data, format="json")
+        self.assertEqual(response.status_code, 405)      
 
-    # # requests by user id
-    # def test_request_by_user_id_get(self):
-    #     self.post()
-    #     res = self.client.get(self.requests_by_user_id(1))
-    #     self.assertEqual(res.status_code, 200)
+    # requests by user id
+    def test_request_by_user_id_get(self):
+        self.post()
+        res = self.client.get(self.requests_by_user_id(1))
+        self.assertEqual(res.status_code, 200)
 
-    # def test_request_by_user_id_post(self):
-    #     response = self.client.post(self.requests_by_user_id(1), self.request_data, format="json")
-    #     self.assertEqual(response.status_code, 405)
+    def test_request_by_user_id_post(self):
+        response = self.client.post(self.requests_by_user_id(1), self.request_data, format="json")
+        self.assertEqual(response.status_code, 405)
 
-    # def test_request_by_user_id_put(self):
-    #     response = self.client.put(self.requests_by_user_id(1), self.request_data, format="json")
-    #     self.assertEqual(response.status_code, 405)
+    def test_request_by_user_id_put(self):
+        response = self.client.put(self.requests_by_user_id(1), self.request_data, format="json")
+        self.assertEqual(response.status_code, 405)
 
-    # def test_request_by_user_id_delete(self):
-    #     response = self.client.delete(self.requests_by_user_id(1), self.request_data, format="json")
-    #     self.assertEqual(response.status_code, 405)    
+    def test_request_by_user_id_delete(self):
+        response = self.client.delete(self.requests_by_user_id(1), self.request_data, format="json")
+        self.assertEqual(response.status_code, 405)    
